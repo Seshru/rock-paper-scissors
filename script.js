79ea83706ecc951c111e6543037f8e61e02d780e
Josh Lepine <josh.j.lepine@gmail.com>
2024-05-06 12:55:39 +0100

    Added function to determine human choice - getHumanChoice() - performs a do while loop to obtain user input using prompt until user enters a valid option

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-
-
 function getComputerChoice() {
-    let choice = Math.random();
-    let final_choice;
-    if (choice < 0.33) {
-        final_choice = "Rock"
-    } else if (choice < 0.66) {
-        final_choice = "Scissors"
-    } else {
-        final_choice = "Paper"
-    }
-    return final_choice;
+	let choice = Math.random();
+	let computer_choice;
+	if (choice < 0.33) {
+		computer_choice = "Rock";
+	} else if (choice < 0.66) {
+		computer_choice = "Scissors";
+	} else {
+		computer_choice = "Paper";
+	}
+	return computer_choice.toLowerCase();
+}
+
+function getHumanChoice() {
+	let human_choice;
+	do {
+		human_choice = prompt("Please enter rock, paper or scissors");
+	} while (
+		human_choice.toLowerCase() !== "rock" &&
+		human_choice.toLowerCase() !== "scissors" &&
+		human_choice.toLowerCase() !== "paper"
+	);
+	console.log(human_choice.toLowerCase());
+	return human_choice.toLowerCase();
 }
